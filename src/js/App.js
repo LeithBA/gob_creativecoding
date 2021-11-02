e5fe050e5396ef2014430a12f334a62ce49dc8a2
LeithBA <34186819+LeithBA@users.noreply.github.com>
2021-11-02 16:35:46 +0100

    Initial commit

---
+++ src/js/App.js
@@ -0,0 +1,80 @@
+import { Scene, sRGBEncoding, WebGLRenderer } from 'three'
+import * as dat from 'dat.gui'
+
+import Sizes from '@tools/Sizes'
+import Time from '@tools/Time'
+import Assets from '@tools/Loader'
+
+import Camera from './Camera'
+import World from '@world/index'
+
+export default class App {
+  constructor(options) {
+    // Set options
+    this.canvas = options.canvas
+
+    // Set up
+    this.time = new Time()
+    this.sizes = new Sizes()
+    this.assets = new Assets()
+
+    this.setConfig()
+    this.setRenderer()
+    this.setCamera()
+    this.setWorld()
+  }
+  setRenderer() {
+    // Set scene
+    this.scene = new Scene()
+    // Set renderer
+    this.renderer = new WebGLRenderer({
+      canvas: this.canvas,
+      alpha: true,
+      antialias: true,
+      powerPreference: 'high-performance',
+    })
+    this.renderer.outputEncoding = sRGBEncoding
+    this.renderer.gammaFactor = 2.2
+    // Set background color
+    this.renderer.setClearColor(0x212121, 1)
+    // Set renderer pixel ratio & sizes
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(this.sizes.viewport.width, this.sizes.viewport.height)
+    // Resize renderer on resize event
+    this.sizes.on('resize', () => {
+      this.renderer.setSize(
+        this.sizes.viewport.width,
+        this.sizes.viewport.height
+      )
+    })
+    // Set RequestAnimationFrame with 60fps
+    this.time.on('tick', () => {
+      this.renderer.render(this.scene, this.camera.camera)
+    })
+  }
+  setCamera() {
+    // Create camera instance
+    this.camera = new Camera({
+      sizes: this.sizes,
+      renderer: this.renderer,
+      debug: this.debug,
+    })
+    // Add camera to scene
+    this.scene.add(this.camera.container)
+  }
+  setWorld() {
+    // Create world instance
+    this.world = new World({
+      time: this.time,
+      debug: this.debug,
+      assets: this.assets,
+    })
+    // Add world to scene
+    this.scene.add(this.world.container)
+  }
+  setConfig() {
+    if (window.location.hash === '#debug') {
+      this.debug = new dat.GUI({ width: 450 })
+    }
+  }
+}
